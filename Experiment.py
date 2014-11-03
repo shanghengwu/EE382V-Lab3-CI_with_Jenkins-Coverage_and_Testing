d9be7e70852ea59b38ff36ac601d2c1b2e3b472c
shanghengwu <shangheng.w@gmail.com>
2014-11-03 07:18:35 -0600

    change spaces in Eeperiment.py version2

--- Experiment.py
+++ Experiment.py
@@ -2,10 +2,10 @@ import sys
 
 def largest(list):
     if len(list) == 0:
-      raise ValueError("Cannot call largest on empty list")
+        raise ValueError("Cannot call largest on empty list")
     max = -sys.maxint # "smallest" possible int
     # max = 0
     for index in range(len(list)):
-      if (list[index] > max):
-        max = list[index]
+        if (list[index] > max):
+            max = list[index]
     return max
